ebe0c333f48c3b8bdb887e304950168eefbd696c
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-09-14 11:56:23 +0200

    types

---
+++ src/main/java/io/skaihen/proyecto/classes/Types.java
@@ -0,0 +1,13 @@
+package io.skaihen.proyecto.classes;
+
+import java.util.List;
+
+public class Types {
+
+    private Types() {
+    };
+
+    public static List<Integer> getTypeStrengthsAndWeaknesses(String type) {
+
+    }
+}
