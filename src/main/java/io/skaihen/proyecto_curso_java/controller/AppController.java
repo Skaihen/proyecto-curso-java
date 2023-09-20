527b37cbc51145cb31cf98aeca37fcd008cf8be1
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-09-20 17:59:00 +0200

    Update AppController.java

--- src/main/java/io/skaihen/proyecto_curso_java/controller/AppController.java
+++ src/main/java/io/skaihen/proyecto_curso_java/controller/AppController.java
@@ -1,8 +1,12 @@
 package io.skaihen.proyecto_curso_java.controller;
 
+import io.skaihen.proyecto_curso_java.entities.Pokemon;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 public class AppController {
     @Value("${spring.application.name}")
@@ -12,5 +16,13 @@ public class AppController {
     public String homePage(Model model) {
         model.addAttribute("appName", appName);
         return "index";
+    }
+
+    @PostMapping("/addPokemon")
+    public String addPokemon(@Valid Pokemon pokemon, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "index";
+        }
+        return "redirect:/";
     }
 }
