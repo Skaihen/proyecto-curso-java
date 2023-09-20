d7509f3790db784b14b88750b686220ef6643350
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-09-20 16:48:24 +0200

    thymeleaf

--- src/main/java/io/skaihen/proyecto_curso_java/ProyectoCursoJavaApplication.java
+++ src/main/java/io/skaihen/proyecto_curso_java/ProyectoCursoJavaApplication.java
@@ -3,11 +3,10 @@ package io.skaihen.proyecto_curso_java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ProyectoCursoJavaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoCursoJavaApplication.class, args);
 	}
-
 }
