f9e80736c9abdeb9d6c2428774b28b5b2cadbc3d
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-09-22 13:48:33 +0200

    router

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from "react";
-
-type Pokemon = {
-  id: number;
-  name: string;
-};
+import { Link } from "react-router-dom";
+import { Button, ButtonGroup } from "reactstrap";
+import { Pokemon } from "./types/pokemonType";
 
 function App() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
@@ -20,18 +18,37 @@ function App() {
       });
   }, []);
 
+  async function remove(id: number) {
+    await fetch(`http://127.0.0.1:8080/api/pokemon/${id}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      const updatedPokemons = [...pokemons].filter((i) => i.id !== id);
+      setPokemons(updatedPokemons);
+    });
+  }
+
   if (loading) {
     return <p>Loading...</p>;
   }
 
   return (
     <div className="p-5">
-      <h1>Pokemon CRUD</h1>
+      <div className="d-flex justify-content-between">
+        <h1>Pokemon CRUD</h1>
+        <Button color="success" tag={Link} to="/pokemons/new">
+          Add Group
+        </Button>
+      </div>
       <table className="table">
         <thead>
           <tr>
             <th scope="col">#</th>
             <th scope="col">Name</th>
+            <th scope="col">Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -39,6 +56,23 @@ function App() {
             <tr key={pokemon.id}>
               <th scope="row">{pokemon.id}</th>
               <td>{pokemon.name}</td>
+              <ButtonGroup>
+                <Button
+                  size="sm"
+                  color="primary"
+                  tag={Link}
+                  to={"/pokemons/" + pokemon.id}
+                >
+                  Edit
+                </Button>
+                <Button
+                  size="sm"
+                  color="danger"
+                  onClick={() => remove(pokemon.id)}
+                >
+                  Delete
+                </Button>
+              </ButtonGroup>
             </tr>
           ))}
         </tbody>
